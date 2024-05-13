0fae985bd8e2c1aced5913c423445145e7c871c6
Jonathan <jonathan.jamorim@gmail.com>
2024-05-13 14:17:03 -0300

    1º desafio projeto Curso Santander 2024

---
+++ Desafio de projeto/Aulas/Index.js
@@ -0,0 +1,21 @@
+let nomHeroi = "Jonathan"
+let XPHeroi = 1
+
+
+if (XPHeroi <= 1000) {
+    console.log ("O Herói de nome " + nomHeroi + " está no nível de Ferro")
+} else if (XPHeroi > 1000 && XPHeroi <= 2000) {
+    console.log ("O Herói de nome " + nomHeroi + " está no nível de Bronze")
+} else if (XPHeroi > 2000 && XPHeroi <= 5000) {
+    console.log ("O Herói de nome " + nomHeroi + " está no nível de Prata")
+} else if (XPHeroi > 5000 && XPHeroi <= 7000) {
+    console.log ("O Herói de nome " + nomHeroi + " está no nível de Ouro")
+} else if (XPHeroi > 7000 && XPHeroi <= 8000) {
+    console.log ("O Herói de nome " + nomHeroi + " está no nível de Platina")
+} else if (XPHeroi > 8000 && XPHeroi <= 9000) {
+    console.log ("O Herói de nome " + nomHeroi + " está no nível de Ascendente")
+} else if (XPHeroi > 9000 && XPHeroi <= 10000) {
+    console.log ("O Herói de nome " + nomHeroi + " está no nível de Imortal")
+} else {
+    console.log ("O Herói de nome " + nomHeroi + " está no nível de Radiante")
+}
